28c14d6623dcc297a9a3bb0965e4bca454aa6e8f
Himsa <himsa@revenus.com>
2022-07-05 17:37:10 +0700

    add theme class

--- lib/theme/index.dart
+++ lib/theme/index.dart
@@ -2,3 +2,4 @@ export 'nag_colors.dart';
 export 'nag_typography.dart';
 export 'nag_spacing.dart';
 export 'nag_theme_extensions.dart';
+export 'nag_theme.dart';
---
+++ lib/theme/nag_theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:nag_ui/nag_ui.dart';
+
+ThemeData getAppTheme(BuildContext _) {
+  return ThemeData(
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    scaffoldBackgroundColor: NAGColors.primary,
+    primarySwatch: getPrimaryMaterialColor(),
+    fontFamily: 'CircularXXTT',
+    buttonTheme: ButtonThemeData(
+      buttonColor: NAGColors.secondary,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+    ),
+    appBarTheme: AppBarTheme(
+      elevation: 0,
+      iconTheme: IconThemeData(
+        color: NAGColors.neutral.shade800,
+      ),
+    ),
+    textTheme: TextTheme(
+      bodyText2: NAGRegularText.body2.neutral70,
+    ),
+    dividerTheme: DividerThemeData(
+      color: NAGColors.neutral.shade200,
+      thickness: 1,
+    ),
+  );
+}
